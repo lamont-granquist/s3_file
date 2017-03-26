ee756be91c7f52d89089673511d57d28736e2f72
Lamont Granquist <lamont@scriptkiddie.org>
2017-03-26 15:20:10 -0700

    default: nil fails validation on chef-13
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- resources/default.rb
+++ resources/default.rb
@@ -6,22 +6,22 @@ attribute :remote_path, kind_of: String, required: true
 attribute :bucket, kind_of: String, required: true
 attribute :aws_access_key_id, kind_of: String, required: true
 attribute :aws_secret_access_key, kind_of: String, required: true
-attribute :owner, regex: Chef::Config[:user_valid_regex], default: nil
-attribute :group, regex: Chef::Config[:group_valid_regex], default: nil
-attribute :mode, kind_of: [Integer, String], default: nil
-attribute :checksum, kind_of: [String], default: nil
-attribute :backup, kind_of: [Integer, FalseClass], default: nil
-attribute :use_etag, kind_of: [TrueClass, FalseClass], default: nil
-attribute :use_etags, kind_of: [TrueClass, FalseClass], default: nil
-attribute :use_last_modified, kind_of: [TrueClass, FalseClass], default: nil
-attribute :use_conditional_get, kind_of: [TrueClass, FalseClass], default: nil
-attribute :headers, kind_of: Hash, default: nil
-attribute :sensitive, kind_of: [TrueClass, FalseClass], default: nil
-attribute :inherits, kind_of: [TrueClass, FalseClass], default: nil
-attribute :rights, kind_of: Hash, default: nil
-attribute :atomic_update, kind_of: [TrueClass, FalseClass], default: nil
-attribute :force_unlink, kind_of: [TrueClass, FalseClass], default: nil
-attribute :manage_symlink_source, kind_of: [TrueClass, FalseClass], default: nil
+attribute :owner, regex: Chef::Config[:user_valid_regex]
+attribute :group, regex: Chef::Config[:group_valid_regex]
+attribute :mode, kind_of: [Integer, String]
+attribute :checksum, kind_of: [String]
+attribute :backup, kind_of: [Integer, FalseClass]
+attribute :use_etag, kind_of: [TrueClass, FalseClass]
+attribute :use_etags, kind_of: [TrueClass, FalseClass]
+attribute :use_last_modified, kind_of: [TrueClass, FalseClass]
+attribute :use_conditional_get, kind_of: [TrueClass, FalseClass]
+attribute :headers, kind_of: Hash
+attribute :sensitive, kind_of: [TrueClass, FalseClass]
+attribute :inherits, kind_of: [TrueClass, FalseClass]
+attribute :rights, kind_of: Hash
+attribute :atomic_update, kind_of: [TrueClass, FalseClass]
+attribute :force_unlink, kind_of: [TrueClass, FalseClass]
+attribute :manage_symlink_source, kind_of: [TrueClass, FalseClass]
 # ftp_active_mode makes no sense with s3 so that attribute of remote_file is ignored
 
 def initialize(*args)
